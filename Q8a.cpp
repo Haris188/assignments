0292ea5069ebb715483225c6f7b0406711437e37
= <=>
2021-11-27 16:54:44 -0800

    q8

--- Q8a.cpp
+++ Q8a.cpp
@@ -2,46 +2,31 @@
 
 using namespace std;
 
-int mirrorPrint(string rows[7][6]){
-    for(int i = 0; i <= 5; i++){
-        for(int j=1; j <= 6; j++){
-            cout << rows[i][j];
-        }
-
-        for(int j=6; j >=1; j--){
-            cout << rows[i][j];
-        }
-        cout << endl;
-    }
-
-    return 0;
-}
-
 int main() {
-    string rows[7][6] = {};
-    int i = 0;
-    while(i <= 5){
+    int i = 1;
+    int threshold = 0;
+
+    while(i<=6){
         int j=1;
 
-        while(j <= 6){
-            if(i < j){
-                rows[i][j] = "!!";
+        while(j<=22){
+            if(j<=threshold){
+                cout << '\\';
+            }
+            else if (j >(22 - threshold)){
+                cout << '\\';
             }
             else{
-                string backslash = "\\\\";
-                rows[i][j] = backslash;
-            }
-
-            if(j==6){
-                rows[i][j] = rows[i][j][0];
+                cout << '!';
             }
 
             j++;
         }
-        i++;
-    }
 
-    mirrorPrint(rows);
+        cout << '\n';
+        threshold = threshold + 2;
+        i++;
+    }    
 
     return 0;
 }
